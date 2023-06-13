4c7c65dcaa6f44616e9c3e989c66d4244ce0306a
Aishwarya <aishwarya.verma28@gmail.com>
2023-06-13 21:42:24 +0530

    auto close submenu

--- src/Components/SubMenu.jsx
+++ src/Components/SubMenu.jsx
@@ -1,34 +1,89 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+// import React, {useState} from 'react'
+// import { Link } from 'react-router-dom'
+
+// const SubMenu = (item) => {
+//     const [open,  setOpen] = useState(false)
+
+//     if(item.subNav){
+//   return (
+//     <div className={open?'sidebar-item open' : 'sidebar-item'}>
+//         <div className='sidebar-title' onClick={() => setOpen(!open)}>
+//             <span>
+//                 {item.title}
+//             </span>
+//             <i className="fa-sharp fa-solid fa-angle-down toggle-btn"></i>
+//         </div>
+//         <div className='sidebar-content'>
+//         { item.subNav.map((child, index) => <SubMenu key={index} {...child} />) }
+//         </div>
+//     </div>
+//   )
+//     }
+
+//     else{
+//         return (
+//             <div className='sidebar-item'>
+//             <Link to={item.path || "#"} className='sidebar-title plain'>
+//                 {item.title}
+//             </Link>
+//             </div>
+//           )     
+//     }
+// }
+
+// export default SubMenu
+
+
+
+
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SubMenu = (item) => {
-    const [open,  setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-    if(item.subNav){
-  return (
-    <div className={open?'sidebar-item open' : 'sidebar-item'}>
-        <div className='sidebar-title' onClick={() => setOpen(!open)}>
-            <span>
-                {item.title}
-            </span>
-            <i className="fa-sharp fa-solid fa-angle-down toggle-btn"></i>
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
+  const handleCloseSubmenu = () => {
+    setOpen(false);
+  };
+
+  if (item.subNav) {
+    return (
+      <div
+        className={open ? 'sidebar-item open' : 'sidebar-item'}
+        onMouseEnter={handleToggle}
+        onMouseLeave={handleCloseSubmenu}
+      >
+        <div className='sidebar-title' onClick={handleToggle}>
+          <span>{item.title}</span>
+          <i className="fa-sharp fa-solid fa-angle-down toggle-btn"></i>
         </div>
-        <div className='sidebar-content'>
-        { item.subNav.map((child, index) => <SubMenu key={index} {...child} />) }
-        </div>
-    </div>
-  )
-    }
+        {open && (
+          <div className='sidebar-content'>
+            {item.subNav.map((child, index) => (
+              <SubMenu key={index} {...child} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div className='sidebar-item'>
+        <Link
+          to={item.path || '#'}
+          className='sidebar-title plain'
+          onClick={handleCloseSubmenu}
+        >
+          {item.title}
+        </Link>
+      </div>
+    );
+  }
+};
 
-    else{
-        return (
-            <div className='sidebar-item'>
-            <Link to={item.path || "#"} className='sidebar-title plain'>
-                {item.title}
-            </Link>
-            </div>
-          )     
-    }
-}
-
-export default SubMenu
+export default SubMenu;
